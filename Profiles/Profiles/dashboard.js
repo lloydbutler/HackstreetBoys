9b0236a0ba5f8009022b7c958e9517d05f7a4ef6
lloydbutler <62755237+lloydbutler@users.noreply.github.com>
2021-08-04 10:33:38 +0100

    Add files via upload

---
+++ Profiles/Profiles/dashboard.js
@@ -0,0 +1,56 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          0,
+          0,
+          6,
+          0,
+          0,
+          0,
+          0
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    
+    options: {
+      scales: { 
+        yAxes: [{
+          ticks: {
+            beginAtZero: false,
+            
+          }
+        }]
+      },
+      
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
